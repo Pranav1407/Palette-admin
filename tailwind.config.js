fe40f465fe6e4c59b5438ec9acf86c775894283d
Pranav1407 <pranavkrishnaa.2003@gmail.com>
2024-12-05 21:33:11 +0530

    ^-^ Fixed vercel build logs ^-^

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-import { stat } from 'fs';
+import tailwindAnimate from "tailwindcss-animate"
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,49 +8,41 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			sidebar: {
-  				'20': 'rgba(2, 159, 230, 0.2)',
-  				'30': 'rgba(2, 159, 230, 0.3)',
-  				DEFAULT: '#029FE6'
-  			},
-  			secondary: '#183867',
-			accTitle: '#818181'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-		fontSize: {
-			stat: '32px',
-			lg: '18px',
-			md: '16px',
-		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    extend: {
+      colors: {
+        sidebar: {
+          '20': 'rgba(2, 159, 230, 0.2)',
+          '30': 'rgba(2, 159, 230, 0.3)',
+          DEFAULT: '#029FE6'
+        },
+        secondary: '#183867',
+        accTitle: '#818181'
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      fontSize: {
+        stat: '32px',
+        lg: '18px',
+        md: '16px',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' }
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' }
+        }
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out'
+      }
+    }
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 }
