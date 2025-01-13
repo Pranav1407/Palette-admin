605b5f4cf7270c2f02e0b10a84b9d560a219d1ca
PranavKrishnaaS <pranavkrishnaa.s@goml.io>
2025-01-13 15:41:04 +0530

    added collapsible sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,55 +1,94 @@
 import { NavLink } from 'react-router-dom'
-import { MdDashboard, MdErrorOutline, MdOutlineCheckCircleOutline, MdEmail, MdPersonAdd, MdOutlineAccessTime  } from 'react-icons/md'
+import { MdErrorOutline, MdOutlineCheckCircleOutline, MdEmail, MdPersonAdd, MdOutlineAccessTime, MdKeyboardArrowDown } from 'react-icons/md'
+import { RxDashboard } from 'react-icons/rx'
+import { PanelRightClose } from 'lucide-react'
+import { useState } from 'react'
 
 const Sidebar = () => {
+    const [isHoardingOpen, setIsHoardingOpen] = useState(false);
+    const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+
     return (
         <div className="flex p-4">
-            <aside className="w-60 bg-sidebar-15 text-sidebar font-normal rounded-[20px] px-4">
-                <div className="flex items-center justify-center py-4">
-                    <img src="/assets/Logo.png" alt="Logo" />
+            <aside className={`${isSidebarCollapsed ? 'w-16' : 'w-60'} transition-all duration-300 bg-sidebar-15 text-sidebar font-normal rounded-[20px] px-4`}>
+                <div className="flex items-center justify-center py-4 cursor-pointer" onClick={() => setIsSidebarCollapsed(!isSidebarCollapsed)}>
+                    {isSidebarCollapsed ? (
+                        <PanelRightClose size={24} />
+                    ) : (
+                        <img src="/assets/Logo.png" alt="Logo" />
+                    )}
                 </div>
                 <nav className="py-4">
                     <ul className="space-y-2 text-md">
-                        <NavLink to="/" className={({ isActive }) => `flex gap-3 py-2 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
-                            <MdDashboard size={25} />
-                            <span>Dashboard</span>
-                        </NavLink>
-                        
-                        {/* <NavLink to="/all-hoardings" className={({ isActive }) => `flex items-center gap-3 py-3 px-10 cursor-pointer hover:bg-white/30 hover:border-l-8 hover:border-secondary ${isActive ? 'bg-white/30 border-l-8 border-secondary' : ''}`}>
-                            <img src="/assets/icons/all-hoarding.svg" alt="All Hoardings" className="w-6 h-6" />
-                            <span>All hoardings</span>
-                        </NavLink> */}
-                        
-                        <NavLink
-                            to="/pending" 
-                            className={({ isActive }) => {
-                                return `flex gap-3 py-3 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`;
-                            }}
+                        <NavLink to="/"  className={({ isActive }) => 
+                            `flex ${isSidebarCollapsed ? 'justify-center py-1' : 'gap-3 pl-2 py-2'} cursor-pointer ${
+                                isActive ? 'bg-white text-secondary rounded-lg' : ''
+                            }`}
                         >
-                            <MdOutlineAccessTime size={25} />
-                            <span>Pending</span>
+                            <RxDashboard size={25} />
+                            {!isSidebarCollapsed && <span>Dashboard</span>}
                         </NavLink>
-                        
-                        <NavLink to="/approved" className={({ isActive }) => `flex gap-3 py-3 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
-                            <MdOutlineCheckCircleOutline size={25} />
-                            <span>Approved</span>
-                        </NavLink>
-                        
-                        <NavLink to="/rejected" className={({ isActive }) => `flex gap-3 py-3 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
-                            <MdErrorOutline size={25} />
-                            <span>Rejected</span>
-                        </NavLink>
+                        <div className="relative">
+                            <div 
+                                onClick={() => setIsHoardingOpen(!isHoardingOpen)}
+                                className={`flex justify-between items-center py-3 cursor-pointer ${isSidebarCollapsed ? 'justify-center pl-1' : 'gap-3 pl-2'}`}
+                            >
+                                <div className="flex gap-3">
+                                    <RxDashboard size={25} />
+                                    {!isSidebarCollapsed && <span>Hoarding</span>}
+                                </div>
+                                {!isSidebarCollapsed && (
+                                    <MdKeyboardArrowDown 
+                                        size={20} 
+                                        className={`transition-transform duration-200 ${isHoardingOpen ? 'rotate-180' : ''}`}
+                                    />
+                                )}
+                            </div>
+                            
+                            {isHoardingOpen && (
+                                <div className={`${isSidebarCollapsed ? 'ml-0' : 'ml-8'} space-y-2`}>
+                                    <NavLink to="/pending"  className={({ isActive }) => 
+                                        `flex ${isSidebarCollapsed ? 'justify-center py-1' : 'gap-3 pl-2 py-2'} cursor-pointer ${
+                                            isActive ? 'bg-white text-secondary rounded-lg' : ''
+                                        }`}
+                                    >
+                                        <MdOutlineAccessTime size={20} />
+                                        {!isSidebarCollapsed && <span>Pending</span>}
+                                    </NavLink>
+                                    
+                                    <NavLink to="/approved" className={({ isActive }) => `flex gap-3 py-2 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
+                                        <MdOutlineCheckCircleOutline size={20} />
+                                        {!isSidebarCollapsed && <span>Approved</span>}
+                                    </NavLink>
+                                    
+                                    <NavLink to="/rejected" className={({ isActive }) => `flex gap-3 py-2 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
+                                        <MdErrorOutline size={20} />
+                                        {!isSidebarCollapsed && <span>Rejected</span>}
+                                    </NavLink>
+                                </div>
+                            )}
+                        </div>
                         
                         <div className="h-px bg-sidebar-30 mx-1 my-2"></div>
                         
-                        <NavLink to="/email-remainder" className={({ isActive }) => `flex gap-3 py-3 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
+                        <NavLink to="/email-remainder" 
+                             className={({ isActive }) => 
+                                `flex ${isSidebarCollapsed ? 'justify-center py-1' : 'gap-3 pl-2 py-2'} cursor-pointer ${
+                                    isActive ? 'bg-white text-secondary rounded-lg' : ''
+                            }`}
+                        >
                             <MdEmail size={25} />
-                            <span>Email remainder</span>
+                            {!isSidebarCollapsed && <span>Email remainder</span>}
                         </NavLink>
                         
-                        <NavLink to="/add-admin" className={({ isActive }) => `flex gap-3 py-3 pl-2 cursor-pointer ${isActive ? 'bg-white text-secondary rounded-lg' : ''}`}>
+                        <NavLink to="/add-admin" 
+                             className={({ isActive }) => 
+                                `flex ${isSidebarCollapsed ? 'justify-center py-1' : 'gap-3 pl-2 py-2'} cursor-pointer ${
+                                    isActive ? 'bg-white text-secondary rounded-lg' : ''
+                            }`}
+                        >
                             <MdPersonAdd size={25} />
-                            <span>Add admin</span>
+                            {!isSidebarCollapsed && <span>Add admin</span>}
                         </NavLink>
                     </ul>
                 </nav>
